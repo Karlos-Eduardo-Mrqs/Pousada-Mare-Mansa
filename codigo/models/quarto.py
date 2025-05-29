4349a8180b0c248d2b0163c129b4efa4ce9f23e8
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2025-05-28 21:01:35 -0300

    Commit changes

--- codigo/models/quarto.py
+++ codigo/models/quarto.py
@@ -1,42 +1,10 @@
 from dataclasses import dataclass
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-@dataclass
-=======
 from tipo import Tipo
->>>>>>> Stashed changes
+@dataclass
 
 @dataclass
 class Quarto:
-<<<<<<< Updated upstream
     numero_quarto:int
     disponibilidade:bool
     capacidade:int
-    # tipo: Tipo
-=======
-    numero_quarto: int
-    disponibilidade: bool
-    capacidade: int
     tipo: 'Tipo'
->>>>>>> Stashed changes
-=======
-from tipo import Tipo
-
-@dataclass
-class Quarto:
-    numero_quarto: int
-    disponibilidade: bool
-    capacidade: int
-    tipo: 'Tipo'
->>>>>>> Stashed changes
-=======
-from tipo import Tipo
-
-@dataclass
-class Quarto:
-    numero_quarto: int
-    disponibilidade: bool
-    capacidade: int
-    tipo: 'Tipo'
->>>>>>> Stashed changes
